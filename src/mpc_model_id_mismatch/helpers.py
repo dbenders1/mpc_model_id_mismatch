d26b2ae8a973eee3053f1e8ae3103266aede7d89
dbenders1 <dbenders@hotmail.nl>
2025-08-05 21:17:16 +0200

    Add plotting helpers functions to helpers

--- src/mpc_model_id_mismatch/helpers.py
+++ src/mpc_model_id_mismatch/helpers.py
@@ -4,6 +4,7 @@ import os
 import yaml
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 from acados_template import (
     AcadosModel,
@@ -368,6 +369,14 @@ def read_file_name(file_name):
     return exp_date, exp_type, exp_platform, exp_goal, exp_details
 
 
+def resize_fig(fig, scale=1):
+    width_in_inches = 245.71811 / 72
+    orig_size = fig.get_size_inches()
+    aspect_ratio = scale * orig_size[1] / orig_size[0]
+    fig.set_size_inches(width_in_inches, width_in_inches * aspect_ratio)
+    new_size = fig.get_size_inches()
+
+
 def rotate_quat(q1, v1):
     ans = quat_mult(
         quat_mult(q1, vertcat(0, v1)),
@@ -411,6 +420,40 @@ def rot_matrix_zyx(rpy_angles):
             ],
         ]
     )
+
+
+def save_fig(fig, fig_path, transparent=False):
+    file_type = fig_path[-3:]
+    fig.savefig(fig_path, format=f"{file_type}", transparent=transparent)
+
+
+def set_fig_properties():
+    props = dict()
+    props["titlepad"] = 4
+    props["tickpad"] = 1
+    props["xlabelpad"] = 0
+    props["ylabelpad"] = 1
+    props["zlabelpad"] = 0
+    props["textsize"] = plt.rcParams["xtick.labelsize"]
+    return props
+
+
+def set_plt_properties():
+    # Plot settings
+    fontsize = 10
+    plt.rcParams["font.family"] = "serif"
+    plt.rcParams["font.serif"] = ["Times New Roman"]
+    plt.rcParams["text.usetex"] = True
+    plt.rcParams["text.latex.preamble"] = r"\usepackage{bm}"
+    plt.rcParams["font.size"] = fontsize
+    plt.rcParams["axes.labelsize"] = fontsize - 2
+    plt.rcParams["axes.titlesize"] = fontsize
+    plt.rcParams["xtick.labelsize"] = fontsize - 4
+    plt.rcParams["ytick.labelsize"] = fontsize - 4
+    plt.rcParams["legend.fontsize"] = fontsize - 4
+    # Set dpi for saving figures
+    plt.rcParams["savefig.dpi"] = 300
+    plt.rcParams["figure.dpi"] = 300
 
 
 def set_start_time_to_zero(*args):
